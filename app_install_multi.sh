af6ca5f6bbf2e491fe9b37dbbe58088e534cf365
Mrinal <122072985+mrinalnath3@users.noreply.github.com>
2025-01-25 21:35:14 +0530

    Update app_install_multi.sh
    updated quates

--- app_install_multi.sh
+++ app_install_multi.sh
@@ -2,32 +2,32 @@
 
 #This line is a comment. This script will allow the installation of applications based on the OS detection using if, elif, and else statements as a universal script.
 
-echo “This script to install git on a Linux distribution”
+echo "This script to install git on a Linux distribution"
 
 echo “****************Installation is starting ******************”
 
 # Setting up the variable upon execution it will detect the OS ID
 
-OS=$(grep “^ID=” /etc/os-release | cut -d’=’ -f2 | tr -d ‘“‘)
+OS=$(grep "^ID=" /etc/os-release | cut -d'=' -f2 | tr -d '"')
 
-if [ “$OS” == “ubuntu” ]
+if [ "$OS" == "ubuntu" ]
 then
-echo “This is ubuntu operating system, installing git…..”
+echo "This is ubuntu operating system, installing git….."
 
 sudo apt install httpd -y
 
-elif [ “$OS” == “centos” ]
+elif [ "$OS" == "centos" ]
 then
 
-echo “This is Centos OS, installing the git……..”
+echo "his is Centos OS, installing the git…….."
 yum install apache2 -y
 
-elif [ “$OS” == “rhel” ]
+elif [ "$OS" == "rhel" ]
 then
-echo “This is not Ubuntu or Centos”
-echo “This is Red Hat Enterprise Linux”
+echo "This is not Ubuntu or Centos"
+echo "This is Red Hat Enterprise Linux"
 sudo dnf install git -y
 else
-echo “OS distribution not matching hence not installing”
+echo "OS distribution not matching hence not installing"
 
 fi
