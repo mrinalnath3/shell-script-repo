3d44ba881514bb49d405f918ac76d662ef0f8a95
Mrinal <122072985+mrinalnath3@users.noreply.github.com>
2025-01-25 20:31:13 +0530

    Create app_install_multi.sh

---
+++ app_install_multi.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+#This line is a comment. This script will allow to installation of applications based on the OS detection using if, elif, and else statements as a universal script.
+
+echo “This script to install git on a Linux distribution”
+
+echo “****************Installation is starting ******************”
+
+# Setting up the variable upon execution it will detect the OS ID
+
+OS=$(grep “^ID=” /etc/os-release | cut -d’=’ -f2 | tr -d ‘“‘)
+
+if [ “$OS” == “ubuntu” ]
+then
+echo “This is ubuntu operating system, installing git…..”
+
+sudo apt install git -y
+
+elif [ “$OS” == “centos” ]
+then
+
+echo “This is Centos OS, installing the git……..”
+yum install git -y
+
+elif [ “$OS” == “rhel” ]
+then
+echo “This is not Ubuntu or Centos”
+echo “This is Red Hat Enterprise Linux”
+sudo dnf install git -y
+else
+echo “OS distribution not matching hence not installing”
+
+fi
